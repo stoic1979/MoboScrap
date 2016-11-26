f3e48891816c94a370bfc9338def52b5cd3787b9
Navjot Singh <weavebytes@gmail.com>
2016-11-26 19:52:07 +0530

    search by amd artist

--- itunes/lookup.py
+++ itunes/lookup.py
@@ -18,7 +18,13 @@ class Lookup:
         print("[%s] search_by_id() url: %s" % (self.tag, lookup_url))
         self._get_page_content(lookup_url)
 
+    def search_by_amg_artist_id(self, id):
+        lookup_url = "%s?amgArtistId=%d" % (self.url, id)
+        print("[%s] search_by_amg_artist_id() url: %s" % (self.tag, lookup_url))
+        self._get_page_content(lookup_url)
+
 
 if __name__ == "__main__":
     lookup = Lookup()
     lookup.search_by_id(909253)
+    lookup.search_by_amg_artist_id(468749)
