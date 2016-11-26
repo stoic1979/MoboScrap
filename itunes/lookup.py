58dddbee7e304fe8e9cf0cf55b7330b932025ac3
Navjot Singh <weavebytes@gmail.com>
2016-11-26 19:44:15 +0530

    get page content for itunes

--- itunes/lookup.py
+++ itunes/lookup.py
@@ -1,3 +1,5 @@
+import requests
+
 class Lookup:
 
     url = "https://itunes.apple.com/lookup"
@@ -7,9 +9,14 @@ class Lookup:
     def __init__(self):
         pass
 
+    def _get_page_content(self, page_url):
+        r = requests.get(page_url)
+        print(r.content)
+
     def search_by_id(self, id):
         lookup_url = "%s?id=%d" % (self.url, id)
         print("[%s] search_by_id() url: %s" % (self.tag, lookup_url))
+        self._get_page_content(lookup_url)
 
 
 if __name__ == "__main__":
